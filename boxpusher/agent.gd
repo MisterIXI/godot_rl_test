1f9458aac3094e7d7faa53318da52966815dcc0d
Yannik Braendle <yannik.braendle@t-online.de>
2024-12-06 18:12:08 +0100

    revamped pusher ai

--- boxpusher/agent.gd
+++ boxpusher/agent.gd
@@ -5,12 +5,15 @@ class_name PusherAgent
 @export var target_area: Area3D
 @export var ai_controller: PusherAIController
 @export var manual_control: bool = false
-var queued_reward = 0.0
 var last_dist = 0.0
 var was_just_reset = false
 const SPEED = 5.0
 const ACCELLERATION = 15.0
 const EPS = 0.1
+
+func _ready():
+	ai_controller.init(self)
+	reset_env()
 
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 	if manual_control:
@@ -28,45 +31,49 @@ func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 		if input_dir.length() != 0:
 			state.linear_velocity = linear_velocity.move_toward(Vector3(input_dir.x, 0, input_dir.y) * SPEED, ACCELLERATION * state.step)
 
-func _physics_process(_delta):
-	# print("position: " + str(position))
-	pass
-
 func move_area_to_random_spot():
 	target_area.position = Vector3(randf_range(-3.8, 3.8), 0, randf_range(-3.8, 3.8))
-	while target_area.position.distance_to(object.position) < 2:
+	while target_area.position.distance_to(object.position) < 2 or target_area.position.distance_to(position) < 2:
 		target_area.position = Vector3(randf_range(-3.8, 3.8), 0, randf_range(-3.8, 3.8))
 	was_just_reset = true
 
 func move_object_to_random_spot():
 	object.position = Vector3(randf_range(-3.8, 3.8), 0, randf_range(-3.8, 3.8))
-	while object.position.distance_to(target_area.position) < 2:
+	while object.position.distance_to(target_area.position) < 2 or object.position.distance_to(position) < 2:
 		object.position = Vector3(randf_range(-3.8, 3.8), 0, randf_range(-3.8, 3.8))
 	was_just_reset = true
 
+func move_agent_to_random_spot():
+	position = Vector3(randf_range(-3.8, 3.8), 0, randf_range(-3.8, 3.8))
+	while position.distance_to(target_area.position) < 2 or position.distance_to(object.position) < 2:
+		position = Vector3(randf_range(-3.8, 3.8), 0, randf_range(-3.8, 3.8))
+	was_just_reset = true
+
 func calc_reward():
-	if is_object_at_wall():
-		move_object_to_random_spot()
-		queued_reward -= 15
 	var distance = object.position.distance_to(target_area.position)
-	var reward = queued_reward
+	var reward = 0
 	var dist_to_cube = object.position.distance_to(position)
-	queued_reward = 0
-	if is_at_wall(): # punish for being in a corner
-		reward -= 1
 	if not was_just_reset: # ignore after teleport of target, because that would punish unnecessarily
 		if distance < last_dist - EPS: # if we're getting closer
-			reward += 1
+			reward += 0.5
 		elif distance > last_dist + EPS: # if we're getting further away
 			reward -= 0.5
 		else: # if we're not moving
-			reward -= 5
-		reward += 5 - dist_to_cube # reward for agent being close to object
-	if was_just_reset:
+			reward -= 0.5
+		reward += max(-10,1 - dist_to_cube)*0.3 # reward for agent being close to object
+	else:
 		was_just_reset = false
 	# reward for being close to object
 	last_dist = distance
 	return reward
+
+func reset_env():
+	move_area_to_random_spot()
+	move_object_to_random_spot()
+	move_agent_to_random_spot()
+	# random object rotation on y
+	object.rotation.y = randf_range(0, 6.28)
+	ai_controller.reward = 0
 
 func is_at_wall():
 	if (position.x > 4.3 or position.x < -4.3) or (position.z > 4.3 or position.z < -4.3):
@@ -80,11 +87,10 @@ func is_object_at_wall():
 
 func _on_target_area_body_entered(body: Node3D) -> void:
 	if body.is_in_group("Object"):
-		# finished objective, move area to random spot
-		move_area_to_random_spot()
-		queued_reward += 10
+		ai_controller.reward += 1
+		ai_controller.done = true
 
 
 func _on_evil_zones_body_entered(body: Node3D) -> void:
 	if body.is_in_group("Agent"):
-		queued_reward -= 0.5
+		ai_controller.reward -= 1
--- boxpusher/pusher_ai_controller.gd
+++ boxpusher/pusher_ai_controller.gd
@@ -1,27 +1,62 @@
-# meta-name: AI Controller Logic
-# meta-description: Methods that need implementing for AI controllers
-# meta-default: true
-extends AIController3D
+extends Node3D
 class_name PusherAIController
 
-@onready var player: PusherAgent = get_parent()
-var move: Vector2 = Vector2.ZERO
-#-- Methods that need implementing using the "extend script" option in Godot --#
+enum ControlModes { INHERIT_FROM_SYNC, HUMAN, TRAINING, ONNX_INFERENCE, RECORD_EXPERT_DEMOS }
+@export var control_mode: ControlModes = ControlModes.INHERIT_FROM_SYNC
+@export var onnx_model_path := ""
+@export var reset_after := 20
 
+@export_group("Record expert demos mode options")
+## Path where the demos will be saved. The file can later be used for imitation learning.
+@export var expert_demo_save_path: String
+## The action that erases the last recorded episode from the currently recorded data.
+@export var remove_last_episode_key: InputEvent
+## Action will be repeated for n frames. Will introduce control lag if larger than 1.
+## Can be used to ensure that action_repeat on inference and training matches
+## the recorded demonstrations.
+@export var action_repeat: int = 1
+
+@export_group("Multi-policy mode options")
+## Allows you to set certain agents to use different policies.
+## Changing has no effect with default SB3 training. Works with Rllib example.
+## Tutorial: https://github.com/edbeeching/godot_rl_agents/blob/main/docs/TRAINING_MULTIPLE_POLICIES.md
+@export var policy_name: String = "shared_policy"
+
+var onnx_model: ONNXModel
+
+var heuristic := "human"
+var done := false
+var reward := 0.0
+var n_steps := 0
+var needs_reset := false
+
+var _player: PusherAgent
+var move := Vector2.ZERO
+
+func _ready():
+	add_to_group("AGENT")
+
+
+func init(player: Node3D):
+	_player = player
+
+
+#-- Methods that need implementing using the "extend script" option in Godot --#
 func get_obs() -> Dictionary:
 	return {"obs":[
-		player.position.x,
-		player.position.z,
-		player.linear_velocity.x,
-		player.linear_velocity.z,
-		player.object.position.x,
-		player.object.position.z,
-		player.target_area.position.x,
-		player.target_area.position.z
+		_player.position.x,
+		_player.position.z,
+		_player.linear_velocity.x,
+		_player.linear_velocity.z,
+		_player.object.position.x,
+		_player.object.position.z,
+		_player.object.rotation_degrees.y / 360,
+		_player.target_area.position.x,
+		_player.target_area.position.z
 	]}
 
 func get_reward() -> float:	
-	return player.calc_reward()
+	return _player.calc_reward() + reward
 	
 func get_action_space() -> Dictionary:
 	return {
@@ -34,16 +69,59 @@ func get_action_space() -> Dictionary:
 func set_action(action) -> void:	
 	move.x = action["move"][0]
 	move.y = action["move"][1]
+
+#-----------------------------------------------------------------------------#
+
+
+#-- Methods that sometimes need implementing using the "extend script" option in Godot --#
+# Only needed if you are recording expert demos with this AIController
+func get_action() -> Array:
+	assert(false, "the get_action method is not implemented in extended AIController but demo_recorder is used")
+	return []
+
 # -----------------------------------------------------------------------------#
 
-#-- Methods that can be overridden if needed --#
 
-#func get_obs_space() -> Dictionary:
-# May need overriding if the obs space is complex
-#	var obs = get_obs()
-#	return {
-#		"obs": {
-#			"size": [len(obs["obs"])],
-#			"space": "box"
-#		},
-#	}
+func _physics_process(_delta):
+	n_steps += 1
+	if n_steps > reset_after:
+		needs_reset = true
+	if needs_reset:
+		reset()
+	reset_if_done()
+
+
+func get_obs_space():
+	# may need overriding if the obs space is complex
+	var obs = get_obs()
+	return {
+		"obs": {"size": [len(obs["obs"])], "space": "box"},
+	}
+
+
+func reset():
+	n_steps = 0
+	_player.reset_env()
+	needs_reset = false
+
+
+func reset_if_done():
+	if done:
+		reset()
+
+
+func set_heuristic(h):
+	# sets the heuristic from "human" or "model" nothing to change here
+	heuristic = h
+
+
+func get_done():
+	return done
+
+
+func set_done_false():
+	done = false
+
+
+func zero_reward():
+	reward = 0.0
